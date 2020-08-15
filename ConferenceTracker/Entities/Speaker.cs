7f352d153039db575f7d9dc906492094aa45e3b5
jslaveva <jslaveva@gmail.com>
2020-08-15 16:17:20 +0300

    Improve Security Using Validation

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,18 +4,47 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
+        [Required]
         public int Id { get; set; }
+        
+        [Required]
+        [DataType(DataType.Text)]
         [Display(Name = "First name")]
+        [StringLength(100, MinimumLength = 2)]
         public string FirstName { get; set; }
-        [Display(Name = "Last name")]
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 2)]
+        [Display(Name = "Last name")]                
         public string LastName { get; set; }
+        
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(500, MinimumLength = 10)]
         public string Description { get; set; }
+
         [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]        
         public string EmailAddress { get; set; }
+        
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+
+            if (!string.IsNullOrEmpty(this.EmailAddress) && this.EmailAddress.EndsWith("TechnologyLiveConference.com"))
+            {
+                results.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+            }
+
+            return results;
+        }
     }
 }
